4b9dce57322ca5dbf3c914aabb7807cb8a67fee1
スタークギャング <StarkGangZ@gmail.com>
2021-03-15 13:45:56 +0530

    Create boy_cott_image_gen.py

---
+++ boy_cott_image_gen.py
@@ -0,0 +1,2 @@
+from main_startup.core.decorators import friday_on_cmd, listen
+from main_startup.helper_func.basic_helpers import edit_or_reply, get_readable_time, delete_or_pass, progress, is_admin_or_owner, get_user, get_text, iter_chats, edit_or_send_as_file
