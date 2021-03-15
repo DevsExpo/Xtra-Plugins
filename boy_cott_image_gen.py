0f174e8ff47ecfcd104e48bb673b00d9c4fe5a0b
スタークギャング <StarkGangZ@gmail.com>
2021-03-15 16:33:18 +0530

    Update boy_cott_image_gen.py

--- boy_cott_image_gen.py
+++ boy_cott_image_gen.py
@@ -1,11 +1,12 @@
 from main_startup.core.decorators import friday_on_cmd, listen
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_readable_time, delete_or_pass, progress, is_admin_or_owner, get_user, get_text, iter_chats, edit_or_send_as_file
 from PIL import Image, ImageDraw, ImageFont, ImageColor
+import os
 from main_startup.helper_func.plugin_helpers import convert_to_image, convert_vid_to_vidnote, generate_meme
 
-@friday_on_cmd(['bcig'])
+@friday_on_cmd(['boycott'])
 async def boycott_kangs(client, message):
-  tgi = await edit_or_reply(message, "`Applying Magic!`")
+  tgi = await edit_or_reply(message, "`Applying BoyCott Magic!`")
   if not message.reply_to_message:
     await tgi.edit("`Please, Reply To Media To Add Boycott Magic!`")
     return
@@ -16,16 +17,18 @@ async def boycott_kangs(client, message):
   if not os.path.exists(img):
       await tgi.edit("`Invalid Media!`")
       return
-  background = Image.open(img)
-  foreground = Image.open("./xtraplugins/helper_files/X.png")
+  background = Image.open(img).convert("RGBA")
+  foreground = Image.open("./xtraplugins/helper_files/x-cross.png").convert("RGBA")
+  x, y = foreground.size
+  foreground = foreground.resize(background.size)
   background.paste(foreground, (0, 0), foreground)
-  file_name = "bcig.png"
-  background.save(file_name, "PNG")
+  file_name = "bcig.webp"
+  background.save(file_name, "WebP")
   if message.reply_to_message:
-        await client.send_photo(message.chat.id, photo = file_name, reply_to_message_id=message.reply_to_message.message_id)
-    else:
-        await client.send_photo(message.chat.id, photo = file_name)
-    await tgi.delete()
-    for files in (file_name, img):
+        await client.send_sticker(message.chat.id, sticker = file_name, reply_to_message_id=message.reply_to_message.message_id)
+  else:
+        await client.send_sticker(message.chat.id, sticker = file_name)
+  await tgi.delete()
+  for files in (file_name, img):
         if files and os.path.exists(files):
             os.remove(files)
