4ebe8e0357ddf81617a9a18c476cb7745f0743ef
スタークギャング <StarkGangZ@gmail.com>
2021-03-22 09:08:33 +0530

    Update booksdl.py

--- booksdl.py
+++ booksdl.py
@@ -16,6 +16,7 @@ import random
 cxc = ["Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36", "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36", "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1", "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246","Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"]
 
 @friday_on_cmd(["booksdl", "bookdl"], 
+is_official=False,
 cmd_help={
         "help": "Downloads Books Just with name!",
         "example": "{ch}booksdl (book name)",
@@ -142,6 +143,7 @@ Title : {TTL.strip()}
     
 
 @friday_on_cmd(["booklinks", "books"], 
+is_official=False,
 cmd_help={
         "help": "Gathers All The Book Download links!",
         "example": "{ch}booklinks (book name)",
@@ -186,16 +188,3 @@ async def bookdl(client, message):
         await client.send_document(message.chat.id, document = open("book.txt", "rb"), caption=f"**BOOKS LINKS GATHERED SUCCESSFULLY!\n\nBY FRIDAY. GET YOUR OWN FRIDAY FROM @FRIDAYCHAT.**")
         os.remove("book.txt")
         await pablo.delete()
-
-
-
-
-_name_ = "BooksDl"
-
-_help_ = """
-**「BooksDl」**
-♦ `{ch}booksdl (book-name)`
-➠ Download Book and Send Just with name.
-♦ `{ch}booklinks (book-name)`
-➠ Gets All The Download Links Of The Book.
-"""
