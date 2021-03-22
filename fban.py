f04bb02635850b07e4f5e0ad6c99bfcb77098e69
スタークギャング <StarkGangZ@gmail.com>
2021-03-22 12:08:44 +0530

    Update fban.py

--- fban.py
+++ fban.py
@@ -132,9 +132,16 @@ async def fetch_all_fed(client, message):
                 return None
             else:
                 X = sed.text
-                Y = X[44:].splitlines()
-                for lol in Y:
-                    fed_list.append(lol)
+                lol = X.splitlines()
+                if "you are the owner" in X.lower():
+                    for lo in lol:
+                        if "you are the owner" not in lo.lower():
+                            if "you are admin" not in lo.lower():
+                                if lo[:36] != '':
+                                    if not lo.startswith("-"):
+                                        fed_list.append(lo[:36])
+                                    else:
+                                        fed_list.append(lo[2:38])
         if sed.media:
             fed_file = await sed.download()
             file = open(fed_file, "r")
