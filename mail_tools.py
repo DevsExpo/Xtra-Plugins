8e425a90dcee8703666c930159356f233833d287
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-03-23 08:23:13 +0530

    Update mail_tools.py

--- mail_tools.py
+++ mail_tools.py
@@ -2,3 +2,11 @@ import requests
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+from main_startup import Friday
+from main_startup.config_var import Config
+from xtraplugins.dB.mail_tools import (
+    add_mail_update_mail,
+    get_msg_id,
+    get_mail_id,
+)
+
