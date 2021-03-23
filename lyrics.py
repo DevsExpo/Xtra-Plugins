525e6c73b6296d7b17466bca4d3e3ef527a53786
スタークギャング <StarkGangZ@gmail.com>
2021-03-23 20:22:29 +0530

    Update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -4,7 +4,7 @@ from tswift import Song
 
 from main_startup.config_var import Config
 from main_startup.core.decorators import friday_on_cmd
-from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+from main_startup.helper_func.basic_helpers import edit_or_reply, get_text, 
 
 
 
@@ -12,35 +12,22 @@ from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
     ["lyrics"],
     cmd_help={
         "help": "This plugin searches for song lyrics with song name",
-        "example": "{ch}lyrics (song name)",
-    },
+        "example": "{ch}lyrics alone",
+    }
 )
 async def _(client,message):
     query = get_text(message)
-    lel = await edit_or_reply(message, "Searching For Lyrics.....")
+    lel = await edit_or_reply(message, "`Searching For Lyrics.....`")
     if not query:
-        await lel.edit("`What I am Supposed to find `")
+        await lel.edit("`Give Me Input!`")
         return
-
-    song = ""
+    song = None
     song = Song.find_song(query)
     if song:
         if song.lyrics:
             reply = song.format()
         else:
-            reply = "Couldn't find any lyrics for that song! try with artist name along with song if still doesnt work try `.glyrics`"
+            reply = "`Lyrics Not Found! Please Work On Your English!`"
     else:
-        reply = "lyrics not found! try with artist name along with song if still doesnt work try `.glyrics`"
-
-    if len(reply) > 4095:
-        with io.BytesIO(str.encode(reply)) as out_file:
-            out_file.name = "lyrics.text"
-            await client.send_document(
-                message.chat.id,
-                out_file,
-                caption=query,
-                reply_to_msg_id=message.message_id,
-            )
-            await lel.delete()
-    else:
-        await lel.edit(reply)
+        reply = "`Lyrics Not Found! Please Work On Your English!`"
+    await edit_or_send_as_file(reply, lel, client, f"Result For {query}", query)
