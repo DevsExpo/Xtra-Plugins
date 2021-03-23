3a3260fbdc4834e22b9c76e1e83b3dd468546e57
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-03-23 13:15:26 +0530

    Update lydia.py

--- dB/lydia.py
+++ dB/lydia.py
@@ -4,22 +4,27 @@ lydia = db_x["LYDIA"]
 
 
 def add_user(chat_id, user_id, string_id):
-    stark = fed.find_one({"chat_id": chat_id, "user_id": user_id, "string_id": string_id})
+    stark = lydia.find_one({"chat_id": chat_id, "user_id": user_id, "string_id": string_id})
     if stark:
         return False
     else:
-        fed.insert_one({"chat_id": chat_id, "user_id": user_id, "string_id": string_id})
+        lydia.insert_one({"chat_id": chat_id, "user_id": user_id, "string_id": string_id})
         return True
 
 
 def remove_user(chat_id, user_id):
-    stark = fed.find_one({"chat_id": chat_id, "user_id": user_id})
+    stark = lydia.find_one({"chat_id": chat_id, "user_id": user_id})
     if not stark:
         return False
     else:
-        fed.delete_one({"chat_id": chat_id, "user_id": user_id, "string_id": string_id})
+        lydia.delete_one({"chat_id": chat_id, "user_id": user_id, "string_id": string_id})
         return True
 
-
+def get_all_users(chat_id):
+    r = list(lydia.find({"chat_id": chat_id}))
+    if r:
+        return r
+    else:
+        return False
 
 
