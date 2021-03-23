0f36c173c58572eb22d81f9c2876e13e06fcbb3b
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-03-23 08:58:05 +0530

    Update mail_tools.py

--- dB/mail_tools.py
+++ dB/mail_tools.py
@@ -3,13 +3,20 @@ from database import db_x
 mail = db_x["MAIL"]
 
 
-def add_mail_update_mail(mail_id, last_msg_id=""):
+def add_mail_update_mail(mail_id, last_msg_id):
     midhun = mail.find_one({"_id": "MAIL_DETAILS")
     if midhun:
         mail.update_one({"_id": "MAIL_DETAILS"}, {"$set": {"mail_id": mail_id}})
         mail.update_one({"_id": "MAIL_DETAILS"}, {"$set": {"last_msg_id": last_msg_id}})
     else:
         mail.insert_one({"_id": "MAIL_DETAILS", "mail_id": mail_id, "last_msg_id": last_msg_id})
+
+def add_msg_update_msg(last_msg_id):
+    midhun = mail.find_one({"_id": "MAIL_DETAILS")
+    if midhun:
+        mail.update_one({"_id": "MAIL_DETAILS"}, {"$set": {"last_msg_id": last_msg_id}})
+    else:
+       return False
 
 
 def get_msg_id(mail_id):
