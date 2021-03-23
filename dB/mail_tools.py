22ae644a921fc7133a3ebc668d8d9e8e705e74b9
スタークギャング <StarkGangZ@gmail.com>
2021-03-23 09:27:57 +0530

    Update mail_tools.py

--- dB/mail_tools.py
+++ dB/mail_tools.py
@@ -6,7 +6,7 @@ mail = db_x["MAIL"]
 def add_mail_update_mail(mail_id, last_msg_id=""):
     midhun = mail.find_one({"_id": "MAIL_DETAILS")
     if midhun:
-        mail.update_one({"_id": "MAIL_DETAILS", "mail_id": mail_id}, {"$set": {"last_msg_id": last_msg_id}})
+        mail.update_one({"_id": "MAIL_DETAILS"}, {"$set": {"last_msg_id": last_msg_id, "mail_id": mail_id}})
     else:
         mail.insert_one({"_id": "MAIL_DETAILS", "mail_id": mail_id, "last_msg_id": last_msg_id})
 
