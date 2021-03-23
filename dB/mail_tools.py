089d98a65c7486f71a273e958fc453243a359d00
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-03-23 08:18:46 +0530

    Update mail_tools.py

--- dB/mail_tools.py
+++ dB/mail_tools.py
@@ -12,3 +12,17 @@ def add_mail_update_mail(mail_id, last_msg_id=""):
         mail.insert_one({"_id": "MAIL_DETAILS", "mail_id": mail_id, "last_msg_id": last_msg_id})
 
 
+def get_msg_id(mail_id):
+midhun = mail.find_one({"_id": "MAIL_DETAILS")
+    if midhun:
+        return midhun["last_msg_id"]
+    else:
+        return False
+
+
+def get_mail_id():
+    midhun = mail.find_one({"_id": "MAIL_DETAILS")
+    if midhun:
+        return midhun["mail_id"]
+    else:
+        return False
