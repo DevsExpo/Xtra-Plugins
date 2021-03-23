8918e7012bd042f52063ddecbbd744224113d5d4
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-03-23 17:40:30 +0530

    Update lydia.py

--- lydia.py
+++ lydia.py
@@ -56,7 +56,7 @@ async def remcf(client, message):
 async def livelydia(client, message):
     if not message.text:
         message.continue_propagation()
-    print(get_session(message.chat.id))
+    print(get_session(int(message.chat.id)))
     print(message.chat.id)
     if not get_session(message.chat.id):
         message.continue_propagation()
