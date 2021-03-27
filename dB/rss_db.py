3b5667ec44a69eeb04248063a2dc36652ce9eda2
chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
2021-03-27 17:29:49 +0530

    Update rss_db.py

--- dB/rss_db.py
+++ dB/rss_db.py
@@ -11,18 +11,18 @@ def del_rss(chat_id, rss_link):
     rss.delete_one({"chat_id": chat_id, "rss_link": rss_link})
 
 def get_chat_rss(chat_id):
-    lol = list(amazon_url.find({"chat_id": chat_id}))
+    lol = list(rss.find({"chat_id": chat_id}))
     return lol
 
 def update_rss(chat_id, rss_link, latest_rss):
     rss.update_one({"chat_id": chat_id, "rss_link": rss_link, {"$set": {"latest_rss": latest_rss}})
 
 def get_last_rss(chat_id, rss_link):
-    lol = amazon_url.find({"chat_id": chat_id, "rss_link": rss_link})
+    lol = rss.find({"chat_id": chat_id, "rss_link": rss_link})
     return lol
 
 def is_get_chat_rss(chat_id, rss_link):
-    lol = amazon_url.find_one({"chat_id": chat_id, "rss_link": rss_link})
+    lol = rss.find_one({"chat_id": chat_id, "rss_link": rss_link})
     if lol:
         return True
     else:
