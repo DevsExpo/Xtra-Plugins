42a5761eef67c59f0895297dcc5635bb056c7f0e
StarkGang <starkgangz@gmail.com>
2021-04-01 11:54:03 +0530

    Add License
    Co-Authored-By: chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
    Co-Authored-By: Aditya <70588365+Aditya-XD@users.noreply.github.com>
    Co-Authored-By: Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
    Co-Authored-By: Lakhac <68181994+Lakhac@users.noreply.github.com>

--- anime_world.py
+++ anime_world.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+#
+# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from anime_downloader.sites import get_anime_class
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
--- github.py
+++ github.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+#
+# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import requests
 
 from main_startup.core.decorators import friday_on_cmd
