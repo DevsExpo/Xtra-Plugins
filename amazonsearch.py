fff59a2938d5a7bf194aec1a045efce0627530ec
スタークギャング <StarkGangZ@gmail.com>
2021-04-01 15:49:03 +0530

    Update amazonsearch.py

--- amazonsearch.py
+++ amazonsearch.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+#
+# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import requests
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
@@ -6,26 +14,22 @@ from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 @friday_on_cmd(
     ["amsearch"],
     cmd_help={
-        "help": "Search Product From Amazon",
+        "help": "Search Products From Amazon!",
         "example": "{ch}amsearch Iphone",
     }
 )
 
-async def _(client,message):
-    msgg = get_text(message)
-
-    sedlife = await edit_or_reply(message, "```Searching Product..```")
-    if not msgg:
-        await sedlife.edit("`Dumb Give Me Input`")
+async def _am_search_by_lackhac(client,message):
+    query = get_text(message)
+    msg_ = await edit_or_reply(message, "`Searching Product!`")
+    if not query:
+        await msg_.edit("`Please, Give Input!`")
         return
-
     product = ""
-    r=requests.get(f"https://amznsearch.vercel.app/api/?query={msgg}").json()
-
+    r = requests.get(f"https://amznsearch.vercel.app/api/?query={msgg}").json()
     for products in r:
         link = products['productLink']
         name = products['productName']
         price= products['productPrice']
         product += f"<a href='{link}'>• {name}\n{price}</a>\n"
-
     await edit_or_reply(message,product,parse_mode="HTML")
