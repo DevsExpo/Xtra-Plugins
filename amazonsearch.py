c7f52d8901c812a961574fee8c7a39e8fc2178d1
Lakhac <68181994+Lakhac@users.noreply.github.com>
2021-04-01 15:32:19 +0530

    Add files via upload

---
+++ amazonsearch.py
@@ -0,0 +1,31 @@
+import requests
+from main_startup.core.decorators import friday_on_cmd
+from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+
+
+@friday_on_cmd(
+    ["amsearch"],
+    cmd_help={
+        "help": "Search Product From Amazon",
+        "example": "{ch}amsearch Iphone",
+    }
+)
+
+async def _(client,message):
+    msgg = get_text(message)
+
+    sedlife = await edit_or_reply(message, "```Searching Product..```")
+    if not msgg:
+        await sedlife.edit("`Dumb Give Me Input`")
+        return
+
+    product = ""
+    r=requests.get(f"https://amznsearch.vercel.app/api/?query={msgg}").json()
+
+    for products in r:
+        link = products['productLink']
+        name = products['productName']
+        price= products['productPrice']
+        product += f"<a href='{link}'>{name}\n{price}</a>\n• "
+
+    await edit_or_reply(message,product,parse_mode="HTML")
