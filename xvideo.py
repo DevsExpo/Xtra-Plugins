b3aa22421308a75297a2654a7fa77fe58c049a6e
Lakhac <68181994+Lakhac@users.noreply.github.com>
2021-04-01 18:31:03 +0530

    Add files via upload

---
+++ xvideo.py
@@ -0,0 +1,32 @@
+import requests
+import bs4
+
+from main_startup.core.decorators import friday_on_cmd
+from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+
+
+@friday_on_cmd(
+    ["xvideo"],
+    cmd_help={
+        "help": "Get direct Downloadable",
+        "example": "{ch}xvideo xvideo_link",
+    },
+)
+async def xvid(client, message):
+    editer= await edit_or_reply(message, "`Please Wait.....`")
+    msg = get_text(message)
+    if not msg:
+            await editer.edit("`Please Enter Valid Input`")
+            return
+    try:
+        req = requests.get(msg)
+        soup = bs4.BeautifulSoup(req.content, 'html.parser')
+
+        soups = soup.find("div",{"id":"video-player-bg"})
+        link =""
+        for a in soups.find_all('a', href=True):
+            link = a["href"]
+        await editer.edit(f"HERE IS YOUR LINK:\n`{link}`")
+    except:
+        await editer.edit("Something went wrong")
+
