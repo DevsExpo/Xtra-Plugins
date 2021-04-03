6f4770356ec6f46e08549f551e80dd05a298ea55
Lakhac <68181994+Lakhac@users.noreply.github.com>
2021-04-03 09:52:42 +0530

    Update xvideo.py

--- xvideo.py
+++ xvideo.py
@@ -31,37 +31,3 @@ async def xvid(client, message):
         await editer.edit("Something went wrong")
 
 
-@friday_on_cmd(
-    ["xsearch"],
-    cmd_help={
-        "help": "Xvideo Searcher",
-        "example": "{ch}xsearch query",
-    },
-)
-
-async def xvidsearch(client, message):
-    editer= await edit_or_reply(message, "`Please Wait.....`")
-    msg = get_text(message)
-    if not msg:
-            await editer.edit("`Please Enter Valid Input`")
-            return
-    try:
-        qu = msg.replace(" ","+")
-        page= requests.get(f"https://www.xvideos.com/?k={qu}").content
-        soup = bs4.BeautifulSoup(page, 'html.parser')
-        col= soup.findAll("div",{"class":"thumb"})
-
-        links= ""
-
-        for i in col:
-            a = i.find("a")
-            link = a.get('href')
-
-            semd = link.split("/")[2]
-
-            links += f"<a href='https://www.xvideos.com{link}'>• {semd.upper()}</a>\n"
-        await editer.edit(links,parse_mode="HTML")
-
-
-    except:
-         await editer.edit("Something Went Wrong")
