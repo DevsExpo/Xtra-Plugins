d601852742919069919c884cede1d5b44d3a0794
StarkGang <starkgangz@gmail.com>
2021-04-05 10:08:11 +0530

    Add SpeedTest Support
    Co-Authored-By: スタークギャング <StarkGangZ@gmail.com>
    Co-Authored-By: chsaiujwal <69893826+chsaiujwal@users.noreply.github.com>
    Co-Authored-By: Aditya <70588365+Aditya-XD@users.noreply.github.com>
    Co-Authored-By: Lakhac <68181994+Lakhac@users.noreply.github.com>
    Co-Authored-By: Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>

---
+++ speedtest.py
@@ -0,0 +1,48 @@
+import speedtest
+from main_startup.core.decorators import friday_on_cmd
+from main_startup.helper_func.basic_helpers import edit_or_reply, get_text, edit_or_send_as_file, get_user
+
+
+@friday_on_cmd(['speedtest', 'st'],
+               cmd_help={
+                'help': 'Test Your Server Speed.',
+                'example': '{ch}speedtest'})
+async def spee_test(client, message):
+    ms_g = await edit_or_reply(message, "`Please Wait, Calculating Server Speed.`")
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    response = s.results.dict()
+    download_speed = response.get("download")
+    upload_speed = response.get("upload")
+    ping_time = response.get("ping")
+    client_infos = response.get("client")
+    img_ = s.results.share()
+    _neat_test = f"""<b><u>SPEED TEST</b></u>
+    
+▶ <b>IP :</b> <code>{client_infos['ip']}</code>
+▶ <b>ISP :</b> <code>{client_infos['isp']}</code>
+▶ <b>Country :</b> <code>{client_infos['country']}</code>
+▶ <b>Download Speed :</b> <code>{download_speed}</code>
+▶ <b>Upload Speed :</b> <code>{upload_speed}</code>
+▶ <b>ISP RATING :</b> <code>{client_infos['isprating']}</code>
+▶ <b>PING TIME :</b> <code>{ping_time}</code>
+"""
+    if img_:
+      if message.reply_to_message:
+        await client.send_photo(
+            message.chat.id,
+            img_,
+            reply_to_message_id=message.reply_to_message.message_id,
+            caption=_neat_test
+        )
+      else:
+        await client.send_photo(
+            message.chat.id,
+            img_,
+            caption=_neat_test
+            )
+      await ms_g.delete()
+    else:
+      await ms_g.edit(_neat_test)
