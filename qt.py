96d99ab925e7714d5c43ab93a950c3d68dca2701
スタークギャング <StarkGangZ@gmail.com>
2021-05-15 20:12:53 +0530

    Imports

--- qt.py
+++ qt.py
@@ -10,6 +10,7 @@ from main_startup.config_var import Config
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
+import asyncio
 
 
 @friday_on_cmd(
