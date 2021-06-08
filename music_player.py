5ce8bce518167308f8cfa7028879e437b16feb36
スタークギャング <StarkGangZ@gmail.com>
2021-06-08 22:46:57 +0530

    Update music_player.py

--- music_player.py
+++ music_player.py
@@ -11,6 +11,7 @@ import logging
 import ffmpeg
 from main_startup import Friday
 import time
+import calender
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 from pytgcalls import GroupCall
