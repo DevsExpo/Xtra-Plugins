351e732efa41858953f6f4e39611a3c808155a99
スタークギャング <StarkGangZ@gmail.com>
2021-07-04 11:51:56 +0530

    Update advice.py

--- advice.py
+++ advice.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+#
+# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 import requests
@@ -14,6 +22,3 @@ async def advice(client, message):
     pablo = await edit_or_reply(message, engine.get_string("PROCESSING"))
     r = requests.get("https://api.adviceslip.com/advice")
     await pablo.edit(r.json()["slip"]["advice"])
-
-
-
